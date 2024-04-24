653a4e7aa4add7107a8621d8ac56b44db180aa32
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-04-24 13:29:32 +0200

    Update 1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,5 +8,15 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> Vector:
-    ''' Function that return a list '''
+    ''' Function that returns a list '''
     return [number async for number in async_generator()][:10]
+
+# Test the function
+async def test_async_comprehension():
+    result = await async_comprehension()
+    print(result)
+
+# Run the test
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(test_async_comprehension())
