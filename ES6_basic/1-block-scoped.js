a7abbf04334e0f90429657d55150922ee796a915
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-05-08 13:23:48 +0200

    Update 1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  let task = true;
+  let task2 = false;
 
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
+  if (!trueOrFalse) {
+    task = false;
+    task2 = true;
   }
 
   return [task, task2];
