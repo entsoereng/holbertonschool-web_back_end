1c8b513851333d910a8bcbbd1670c6b15e33768d
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-05-24 12:48:01 +0200

    Update 0-get_list_students.js

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,11 @@
-// 0-get_list_students.js
-
 function getListStudents() {
-  return [
+  const obj = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+
+  return obj;
 }
 
 export default getListStudents;
